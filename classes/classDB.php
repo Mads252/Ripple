516a709c5c9ab2198835f6ded1265074acedee2c
madstofteriksen <mae002@edu.zealand.dk>
2024-09-26 09:25:41 +0200

    Revert "SignUp og logIn"
    This reverts commit 4eb5ea48f77939996be1c414d2fb6c3f517378a0.

--- classes/classDB.php
+++ classes/classDB.php
@@ -140,17 +140,6 @@ class db {
         }
 
     }
-    public function openPDO(){
-        try{
-            $username = "madstofteriksen_dk";
-            $password = "DEbh4amBrexR95f2wFny";
-            $dbh = new PDO('mysql:host=mysql11.unoeuro.com;dbname=madstofteriksen_dk_db_ripple', $username, $password);
-            return $dbh;
-        }
-        catch (PDOException $e){
-            print "error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-    }
+
 }
 ?>
