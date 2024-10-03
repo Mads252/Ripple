a60b41327cc0645b0d4874bd4263bb664a277926
ohjenssen <oskar_jenssen@hotmail.com>
2024-10-03 19:04:25 +0200

    index

--- index.php
+++ index.php
@@ -14,38 +14,38 @@
 
 
 
-    if(!empty($_POST["data"])){
-        $data = $_POST["data"];
+    // if(!empty($_POST["data"])){
+    //     $data = $_POST["data"];
 
-        if(isset($_FILES['data']['tmp_name']['postImage']) && !empty($_FILES['data']['tmp_name']['postImage'])){
-            $imageData = file_get_contents($_FILES['data']['tmp_name']['postImage']);
-        } else {
-            $imageData = null; // Handle case where no image was uploaded
-        }
+    //     if(isset($_FILES['data']['tmp_name']['postImage']) && !empty($_FILES['data']['tmp_name']['postImage'])){
+    //         $imageData = file_get_contents($_FILES['data']['tmp_name']['postImage']);
+    //     } else {
+    //         $imageData = null; // Handle case where no image was uploaded
+    //     }
 
-        if(isset($_SESSION["userId"])) {
-            $user_id = $_SESSION["userId"];
-        }
+    //     if(isset($_SESSION["userId"])) {
+    //         $user_id = $_SESSION["userId"];
+    //     }
 
 
-        $sql = "INSERT INTO posts (users_id, textContent, postImage) VALUES (:user_id, :textContent, :postImage)";
-        $bind = [
-            ":user_id" => $user_id,
-            ":textContent" => $data["textContent"], 
-            ":postImage" => $imageData
-        ];
+    //     $sql = "INSERT INTO posts (users_id, textContent, postImage) VALUES (:user_id, :textContent, :postImage)";
+    //     $bind = [
+    //         ":user_id" => $user_id,
+    //         ":textContent" => $data["textContent"], 
+    //         ":postImage" => $imageData
+    //     ];
 
-        $db->sql($sql, $bind, false);
+    //     $db->sql($sql, $bind, false);
 
-        $post_id = $db->lastInsertId();
-        $sql = "INSERT INTO user_posts (user_connection_id, post_connection_id) VALUES (:user_id, :post_id)";
-        $bind = [
-            ":user_id" => $user_id,
-            ":post_id" => $post_id,
-        ];
+    //     $post_id = $db->lastInsertId();
+    //     $sql = "INSERT INTO user_posts (user_connection_id, post_connection_id) VALUES (:user_id, :post_id)";
+    //     $bind = [
+    //         ":user_id" => $user_id,
+    //         ":post_id" => $post_id,
+    //     ];
 
-        $db->sql($sql, $bind, false);
-    }
+    //     $db->sql($sql, $bind, false);
+    // }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -61,7 +61,35 @@
             <?php profileComponent($username, $loggedin, $image_path)?>
         </section>
 
-        <form class="formContainer" action="index.php" method="post" enctype="multipart/form-data">
+        <section class="container">
+            <h1>Feed</h1>
+
+            <?php 
+            $sqlCall = "SELECT * FROM posts";
+            $posts = $db->sql($sqlCall);
+
+            foreach($posts as $post){
+                ?>
+                <div class="post">
+                        <p class="postText"><?php echo $post->textContent?></p>
+
+                <?php if(!empty($post->postImage)): ?>
+                    <div class="imgContainer">
+                        <img class="postImg" src="data:image/jpeg;base64,<?php echo base64_encode($post->postImage); ?>" alt="<?php echo $post->textContent?> ">
+                    </div>
+                    <?php endif; ?>
+                    <a href="likePost.php?id=<?php echo $post->post_id ?>">Like</a>
+                    <form method="post" action="includes/likePostIncludes.php">
+                        <input type="hidden" name="post_id" value="<?php echo $post->post_id; ?>">
+                        <button type="submit" name="submit">Like this post</button>
+                    </form>
+                </div>
+                <?php
+            }
+        ?>
+        </section>
+
+        <!-- <form class="formContainer" action="index.php" method="post" enctype="multipart/form-data">
             <h2>Add post</h2>
             <div class="textContentContainer">
                 <label class="labels" for="textContentInput">Subject</label>
@@ -74,44 +102,7 @@
             </div>
 
             <button type="submit" class="submitBtn">Add post</button>
-        </form> 
+        </form>  -->
 
-    <section>
-		<div class="sign-up">
-            <form class="formContainer" action="includes/signUpIncludes.php" method="post">
-                <h2>Lav en bruger</h2>
-                <div>
-                    <label class="labels" for="userId">Navn</label>
-                    <input type="text" name="userId" placeholder="Navn">
-                </div>
-                <div>
-                    <label class="labels" for="password">Password</label>
-                    <input type="password" name="password" placeholder="Kodeord">
-                </div>
-                <div>
-                    <label class="labels" for="passwordRepeat">Repeat password</label>
-                    <input type="password" name="passwordRepeat" placeholder="Gentag Kodeord">
-                </div>
-                <div>
-                    <label class="labels" for="email">Email</label>
-                    <input type="text" name="email" placeholder="Email@">
-                </div>
-				<br>
-				<button class="submitBtn" type="submit" name="submit">Lav din bruger</button>
-			</form>
-		</div>
-
-		<div class="log-in">
-            <form class="formContainer" action="includes/logInIncludes.php"  method="post">
-                <h2>Log in</h2>
-				<input type="text" name="userId" placeholder="Navn">
-				<input type="password" name="password" placeholder="Kodeord">
-				<br>
-				<button class="submitBtn" type="submit" name="submit">Log in</button>
-			</form>
-		</div>
-
-		
-	</section>
 </body>
 </html>
